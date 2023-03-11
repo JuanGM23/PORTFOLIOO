589498599e77d409cbc28c41e1f1748acd8288af
JuanGM23 <122891807+JuanGM23@users.noreply.github.com>
2023-03-11 04:24:10 +0100

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,52 @@
-function filtrarProyectos() {
-    var filtro = document.getElementById('filtro');
-    var proyectos = document.getElementById('proyectos');
+const asuntoInput = document.getElementById('asunto');
+const ccInput = document.getElementById('cc');
+const cuerpoInput = document.getElementById('cuerpo');
 
-    var tecnologiaSeleccionada = filtro.value;
-
-    var cards = proyectos.getElementsByClassName('card');
-
-    for (var i = 0; i < cards.length; i++) {
-        var card = cards[i];
-
-        var tecnologias = card.getAttribute('data-tecnologias');
-
-        if (tecnologiaSeleccionada === 'todos' || tecnologias.includes(tecnologiaSeleccionada)) {
-            card.style.display = 'inline-block';
+const validateInput = (inputElement, validationFunction) => {
+    const inputValue = inputElement.value;
+    const isValid = validationFunction(inputValue);
+    if (isValid) {
+        inputElement.classList.remove('invalid');
+        inputElement.classList.add('valid');
+        const errorElement = inputElement.nextElementSibling;
+        if (errorElement && errorElement.classList.contains('error')) {
+            errorElement.remove();
+        }
+    } else {
+        inputElement.classList.remove('valid');
+        inputElement.classList.add('invalid');
+        const errorElement = inputElement.nextElementSibling;
+        if (errorElement && errorElement.classList.contains('error')) {
+            errorElement.textContent = 'Este campo no cumple los requisitos.';
         } else {
-            card.style.display = 'none';
+            const newErrorElement = document.createElement('div');
+            newErrorElement.classList.add('error');
+            newErrorElement.textContent = 'Este campo no cumple los requisitos.';
+            inputElement.parentNode.insertBefore(newErrorElement, inputElement.nextSibling);
         }
     }
-}
+};
+
+const validateAsunto = (asunto) => {
+    return asunto.trim() !== '';
+};
+
+const validateCC = (cc) => {
+    return cc.trim() === '' || /\S+@\S+\.\S+/.test(cc);
+};
+
+const validateCuerpo = (cuerpo) => {
+    return cuerpo.trim() !== '';
+};
+
+asuntoInput.addEventListener('blur', () => {
+    validateInput(asuntoInput, validateAsunto);
+});
+
+ccInput.addEventListener('blur', () => {
+    validateInput(ccInput, validateCC);
+});
+
+cuerpoInput.addEventListener('blur', () => {
+    validateInput(cuerpoInput, validateCuerpo);
+});
